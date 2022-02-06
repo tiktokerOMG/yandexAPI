1680ccad577c73e04ca25a197a00c69059499897
user <antonkotelnikov1976@mail.ru>
2022-02-06 17:07:36 +0300

    task2

--- quest1.py
+++ quest1.py
@@ -37,20 +37,14 @@ object_y = object_delta(toponym)[1]
 
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([object_x, object_y]),
     "l": "map",
     "pt": f"{toponym_coodrinates.split()[0]},{toponym_coodrinates.split()[1]},"
-          f"pm2vvm"
+          f"pm2vvm",
+    "z": 16,
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
 # Запишем полученное изображение в файл.
 map_file = "map.png"
@@ -61,11 +55,39 @@ with open(map_file, "wb") as file:
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+response = requests.get(map_api_server, params=map_params)
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+
+def refresh():
+    response = requests.get(map_api_server, params=map_params)
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    screen.blit(pygame.image.load("map.png"), (0, 0))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            print('hello1')
+            if event.key == pygame.K_PAGEUP:
+                print('hello')
+                map_params["z"] += 1
+                refresh()
+            elif event.key == pygame.K_PAGEDOWN:
+                map_params["z"] -= 1
+                refresh()
+    refresh()
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
