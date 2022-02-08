01e8ed06b5e7f75d70a0e31daee0cc2250e1ece2
user <antonkotelnikov1976@mail.ru>
2022-02-08 15:32:03 +0300

    task4

--- quest1.py
+++ quest1.py
@@ -6,6 +6,26 @@ import requests
 
 from object_delta import object_delta
 
+
+class button:
+    def __init__(self, position, size, text=''):
+        self.clr = (255, 255, 255)
+        self.size = size
+        self.surf = pygame.Surface(size)
+        self.rect = self.surf.get_rect(center=position)
+
+
+        self.font = pygame.font.SysFont('Arial', 15)
+        self.txt = text
+        self.txt_surf = self.font.render(self.txt, 1, (0, 0, 0))
+        self.txt_rect = self.txt_surf.get_rect(center=[wh // 2 for wh in self.size])
+
+    def draw(self, screen):
+        self.surf.fill(self.clr)
+        self.surf.blit(self.txt_surf, self.txt_rect)
+        screen.blit(self.surf, self.rect)
+
+
 toponym_to_find = 'г. Бугульма, ул. Мусы Джалиля, 60'
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
@@ -72,15 +92,23 @@ def refresh():
         sys.exit(1)
     screen.blit(pygame.image.load("map.png"), (0, 0))
 
+
+button1 = button((25, 435), (50, 25), text='схема')
+button2 = button((77, 435), (50, 25), text='спутник')
+button3 = button((129, 435), (50, 25), text='гибрид')
+
+button_list = [button1, button2, button3]
+
+
 running = True
+
+map_type = 1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            print('hello1')
             if event.key == pygame.K_PAGEUP:
-                print('hello')
                 map_params["z"] += 1
             elif event.key == pygame.K_PAGEDOWN:
                 map_params["z"] -= 1
@@ -96,6 +124,23 @@ while running:
             elif event.key == pygame.K_UP:
                 toponym_lattitude += 0.002
                 map_params["ll"] = ",".join([str(i) for i in (toponym_longitude, toponym_lattitude)])
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                for i in button_list:
+                    if i.rect.collidepoint(pos):
+                        if i == button1 and map_type != 1:
+                            map_params["l"] = "map"
+                        elif i == button2 and map_type != 2:
+                            map_params["l"] = "sat"
+                        elif i == button3 and map_type != 3:
+                            map_params["l"] = "sat,skl"
+
+    for b in button_list:
+        b.draw(screen)
+
+        pygame.display.update()
     refresh()
     # Переключаем экран и ждем закрытия окна.
     pygame.display.flip()
